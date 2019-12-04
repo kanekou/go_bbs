3d52dad295a1e5630772614306530e6d8feabe05
kanekou <kanekou@koheinomac-mini.lan>
2019-12-04 13:28:09 +0900

    投稿一覧の取得

--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"html/template"
 	"log"
 	"net/http"
 	"os"
 )
 
 type User struct {
+	Id      int
 	Name    string
-	Age     int
-	Comment string
+	Email   string
+	Message string
 }
 
 func main() {
@@ -22,39 +24,61 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", handler)
+	//mux := http.NewServeMux()
+	//http.Handle("/", http.FileServer(http.Dir("public")))
+	http.HandleFunc("/index", staticHandler)
 	s := &http.Server{
-		Addr:    ":9000",
-		Handler: mux,
+		Addr: ":9000",
+		//Handler: mux,
 	}
 
 	fmt.Println("server start port:8080")
 	s.ListenAndServe()
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	// db接続周り
+func dbConnect() (db *sql.DB) {
 	db, err := sql.Open("mysql", "root:"+os.Getenv("DB_PASSWORD")+"@tcp(127.0.0.1:3306)/rss")
 	if err != nil {
 		log.Fatal(err)
 	}
+	return db
+}
+
+func staticHandler(w http.ResponseWriter, r *http.Request) {
+	db := dbConnect()
 	defer db.Close()
 
-	id := 1
-	var name string
-	err = db.QueryRow("select name from boards where id = ?", id).Scan(&name)
+	// クエリ発行
+	result, err := db.Query("select * from boards")
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Print(name)
+	defer result.Close()
 
-	//user := User{
-	//	Name: name,
-	//	Age: 20,
-	//	Comment: "Hello",
-	//}
-	//tmpl := template.Must(template.ParseFiles("./views/index.html"))
-	//tmpl.Execute(w, user)
+	users := make([]User, 0)
+	for result.Next() {
+		var user User
+		if err := result.Scan(&user.Id, &user.Name, &user.Email, &user.Message); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(user)
+		users = append(users, user)
+	}
+
+	tmpl := template.Must(template.ParseFiles("public/index.html"))
+	tmpl.Execute(w, users)
 	log.Printf("%+v¥n", r)
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+	db := dbConnect()
+	defer db.Close()
+
+	// Formデータを取得.
+	form := r.PostForm
+	fmt.Fprintf(w, "フォーム：\n%v\n", form)
+
+	// または、クエリパラメータも含めて全部.
+	params := r.Form
+	fmt.Fprintf(w, "フォーム2：\n%v\n", params)
 }
