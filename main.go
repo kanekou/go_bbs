bb86bdc999425c401d34fefd8339b11cb13cb99e
kanekou <kanekou@koheinomac-mini.lan>
2019-12-05 14:49:51 +0900

    ログイン機能を実装中。

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"github.com/go-delve/delve/service/api"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"html/template"
@@ -24,6 +25,7 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 
+	http.HandleFunc("/login", loginHandler)
 	http.HandleFunc("/index", staticHandler)
 	s := &http.Server{
 		Addr: ":9000",
@@ -39,6 +41,56 @@ func dbConnect() (db *sql.DB) {
 		log.Fatal(err)
 	}
 	return db
+}
+
+//func checkAuth(r *http.Request) bool {
+//	user, pw, ok := r.BasicAuth()
+//	if !ok || user != autuUser || pw != authPw {
+//		return false
+//	}
+//	return true
+//}
+//
+//func basicAuthHandler(w http.ResponseWriter, r *http.Request) {
+//	if checkAuth(r) == false {
+//		w.Header().Add("WWW.Authenticate", `Basic realm="my private area"`)
+//		w.WriteHeader(http.StatusUnauthorized)
+//		w.Write([]byte("401 Not Authenticate"))
+//		return
+//	}
+//
+//	tmpl := template.Must(template.ParseFiles("public/login.html"))
+//	tmpl.Execute(w, )
+//}
+
+//func Secret(user, realm string) string {
+//	db := dbConnect()
+//	defer db.Close()
+//	login, err := db.Prepare("select * from users where email = ? and password = ?")
+//	if err != nil {
+//		log.Fatal(err)
+//	}
+//	login.Exec(user, email)
+//}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		http.FileServer(http.Dir("public/login.html"))
+	case http.MethodPost:
+		//TODO: email, pw をparamsから持ってくる
+		db := dbConnect()
+		defer db.Close()
+		login, err := db.Prepare("select * from users where email = ? and password = ?")
+		if err != nil {
+			log.Fatal(err)
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+		login.Exec(email, pw)
+		http.Redirect(w, r, "/index", http.StatusFound)
+	default:
+		fmt.Fprint(w, "Method not allowed.\n")
+	}
 }
 
 func staticHandler(w http.ResponseWriter, r *http.Request) {
@@ -89,5 +141,4 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 	default:
 		fmt.Fprint(w, "Method not allowed.\n")
 	}
-
 }
