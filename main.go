3f7e29cd6c3b68df07fa8425bba8af5617e6852b
kanekou <kanekou@koheinomac-mini.lan>
2019-12-05 11:37:04 +0900

    投稿機能の実装をしました。

--- main.go
+++ main.go
@@ -9,7 +9,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"strings"
 )
 
 type User struct {
@@ -26,7 +25,6 @@ func main() {
 	}
 
 	http.HandleFunc("/index", staticHandler)
-	http.HandleFunc("/post", postHandler)
 	s := &http.Server{
 		Addr: ":9000",
 	}
@@ -36,7 +34,7 @@ func main() {
 }
 
 func dbConnect() (db *sql.DB) {
-	db, err := sql.Open("mysql", "root:"+os.Getenv("DB_PASSWORD")+"@tcp(127.0.0.1:3306)/rss")
+	db, err := sql.Open("mysql", "root:"+os.Getenv("DB_PASSWORD")+"@tcp(127.0.0.1:3306)/bbs")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -47,62 +45,35 @@ func staticHandler(w http.ResponseWriter, r *http.Request) {
 	db := dbConnect()
 	defer db.Close()
 
-	//get
-	result, err := db.Query("select * from boards")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer result.Close()
-
-	users := make([]User, 0)
-	for result.Next() {
-		var user User
-		if err := result.Scan(&user.Id, &user.Name, &user.Email, &user.Message); err != nil {
+	if r.Method == http.MethodGet {
+		result, err := db.Query("select * from boards")
+		if err != nil {
 			log.Fatal(err)
 		}
-		users = append(users, user)
+		defer result.Close()
+
+		users := make([]User, 0)
+		for result.Next() {
+			var user User
+			if err := result.Scan(&user.Id, &user.Name, &user.Email, &user.Message); err != nil {
+				log.Fatal(err)
+			}
+			users = append(users, user)
+		}
+
+		tmpl := template.Must(template.ParseFiles("public/index.html"))
+		tmpl.Execute(w, users)
+		log.Printf("%+v¥n", r)
+	} else if r.Method == http.MethodPost {
+		name := r.FormValue(("name"))
+		email := r.FormValue("email")
+		message := r.FormValue("message")
+		insert, err := db.Prepare("insert into boards(name, email, message) values (?,?,?)")
+		if err != nil {
+			log.Fatal(err)
+		}
+		insert.Exec(name, email, message)
+
+		http.Redirect(w, r, "/index", http.StatusFound)
 	}
-
-	tmpl := template.Must(template.ParseFiles("public/index.html"))
-	tmpl.Execute(w, users)
-	log.Printf("%+v¥n", r)
-
-	//post
-	name := r.FormValue(("name"))
-	email := r.FormValue("email")
-	message := r.FormValue("message")
-	insert, err := db.Prepare(" insert into boards(name, email, message) values (?,?,?)")
-	if err != nil {
-		log.Fatal(err)
-	}
-	insert.Exec(name, email, message)
-}
-
-func postHandler(w http.ResponseWriter, r *http.Request) {
-	db := dbConnect()
-	defer db.Close()
-
-	r.ParseForm()
-
-	// Formデータを取得.
-	//fmt.Fprintf(w, "フォーム：\n%v\n", form)
-
-	//name := r.Form.Get("name")
-	//email := r.Form.Get("email")
-	//message := r.Form.Get("message")
-
-	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello astaxie!")
-
-	//if len(form["name"][0]) == 0 || len(form["email"][0]) == 0 || len(form["message"][0] == 0 {
-	//	log.Fatal("値を入力してください")
-	//}
 }
