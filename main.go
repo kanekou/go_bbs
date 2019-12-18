8ef07ab0402218ab70db49b84de32df72eb16ade
kanekou <kanekou@koheinomac-mini.lan>
2019-12-18 12:11:14 +0900

    ログインした状態で/signupに行くと/indexに飛ぶように設定

--- main.go
+++ main.go
@@ -241,15 +241,25 @@ func DbConnect() (db *sql.DB) {
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	//sess, _ := globalSessions.SessionStart(w, r)
 	sess := globalSessions.SessionStart(w, r)
+	db := DbConnect()
+	defer db.Close()
+
 	switch r.Method {
 	case http.MethodGet:
 		tmpl := template.Must(template.ParseFiles("public/login.html"))
 		w.Header().Set("Content-Type", "text/html")
+
+		var name string //TODO: 活用方法
 		email := sess.Get("email")
-		fmt.Println(email)
-		tmpl.Execute(w, email)
+
+		err := db.QueryRow("select name from users where email = ?", email).Scan(&name)
+		if err != sql.ErrNoRows { // Empty set
+			log.Println(err)
+			http.Redirect(w, r, "/index", http.StatusFound)
+		}
+
+		tmpl.Execute(w, nil)
 	case http.MethodPost:
 		var id string
 		email := r.FormValue("email")
@@ -258,14 +268,11 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("email:", r.Form["email"])
 		sess.Set("email", r.Form["email"])
 
-		db := DbConnect()
-		defer db.Close()
 		err := db.QueryRow("select id from users where email = ? and password = ?", email, pw).Scan(&id)
 		if err == sql.ErrNoRows { // Empty set
 			log.Println(err)
 			http.Redirect(w, r, "/login", http.StatusFound)
 		}
-		fmt.Println(id)
 		http.Redirect(w, r, "/index", http.StatusFound)
 	default:
 		w.WriteHeader(http.StatusMethodNotAllowed) // 405
@@ -280,6 +287,14 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodGet:
+		sess := globalSessions.SessionStart(w, r)
+		email := sess.Get("email")
+
+		err := db.QueryRow("select email from users where email = ?", email).Scan(&email)
+		if err != sql.ErrNoRows {
+			http.Redirect(w, r, "/index", http.StatusFound)
+		}
+
 		tmpl := template.Must(template.ParseFiles("public/signup.html"))
 		tmpl.Execute(w, nil)
 	case http.MethodPost:
@@ -303,12 +318,21 @@ func SignupHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func StaticHandler(w http.ResponseWriter, r *http.Request) {
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	db := DbConnect()
 	defer db.Close()
 
 	switch r.Method {
 	case http.MethodGet:
+		sess := globalSessions.SessionStart(w, r)
+		email := sess.Get("email")
+
+		err := db.QueryRow("select email from users where email = ?", email).Scan(&email)
+		if err == sql.ErrNoRows {
+			log.Println(err)
+			http.Redirect(w, r, "/login", http.StatusFound)
+		}
+
 		result, err := db.Query("select * from boards")
 		if err != nil {
 			log.Println(err)
@@ -361,7 +385,7 @@ func main() {
 
 	http.HandleFunc("/login", LoginHandler)
 	http.HandleFunc("/signup", SignupHandler)
-	http.HandleFunc("/index", StaticHandler)
+	http.HandleFunc("/index", IndexHandler)
 	s := &http.Server{
 		Addr: ":9000",
 	}
